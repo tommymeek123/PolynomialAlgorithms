4b91c69ac6ceffeecd786ae3cfad16a93aabca35
thmeek1 <thmeek1@catamount.wcu.edu>
2022-12-08 15:17:49 -0500

    abreviated Mon and Coef inside their respective modules

--- buchberger/src/BaseRing.hs
+++ buchberger/src/BaseRing.hs
@@ -5,18 +5,20 @@ import Data.Ratio ((%), numerator, denominator)
 import qualified RingParams as RP
 import PolyParsers (Readable(..), ratFromString, ratToString)
 
+type Coef = Coefficient
+
 data Coefficient :: RP.Ring -> * where
     Q :: Rational -> Coefficient r
     Fp :: Coefficient r
     deriving Eq
 
-instance Show (Coefficient RP.Q) where
+instance Show (Coef RP.Q) where
     show (Q r) = ratToString r
 
 --instance NFData Field where
 --    rnf (Q a) = rnf a
 
-instance Num (Coefficient RP.Q) where
+instance Num (Coef RP.Q) where
     (Q r) + (Q s) = Q (r + s)
     (Q r) - (Q s) = Q (r - s)
     (Q r) * (Q s) = Q (r * s)
@@ -24,17 +26,17 @@ instance Num (Coefficient RP.Q) where
     signum (Q r)  = Q (signum r)
     fromInteger n = Q $ n % 1
 
-instance Fractional (Coefficient RP.Q) where
+instance Fractional (Coef RP.Q) where
     recip (Q r)  = Q (denominator r % numerator r)
     fromRational = Q
 
-instance Readable (Coefficient RP.Q) where
+instance Readable (Coef RP.Q) where
     fromString = Q . ratFromString
 
-instance Show (Coefficient RP.Fp) where
+instance Show (Coef RP.Fp) where
     show _ = show RP.Fp
 
-instance Num (Coefficient RP.Fp) where
+instance Num (Coef RP.Fp) where
     a + b         = Fp
     a - b         = Fp
     a * b         = Fp
@@ -42,9 +44,9 @@ instance Num (Coefficient RP.Fp) where
     signum a      = Fp
     fromInteger n = Fp
 
-instance Fractional (Coefficient RP.Fp) where
+instance Fractional (Coef RP.Fp) where
     recip a        = Fp
     fromRational a = Fp
 
-instance Readable (Coefficient RP.Fp) where
+instance Readable (Coef RP.Fp) where
     fromString s = Fp
--- buchberger/src/DenseMonom.hs
+++ buchberger/src/DenseMonom.hs
@@ -11,21 +11,23 @@ import qualified Data.Vector.Fixed.Unboxed as UV
 import qualified RingParams as RP
 import PolyParsers (Readable(..), monListFromString, monListToString)
 
+type Mon = Monomial
+
 newtype Monomial :: RP.MonOrder -> Nat -> * where
-    Monomial :: { degVec :: UV.Vec n Int } -> Monomial o n
+    MakeMon :: { degVec :: UV.Vec n Int } -> Monomial o n
 
-deriving instance V.Arity n => Eq (Monomial o n)
+deriving instance V.Arity n => Eq (Mon o n)
 
-instance V.Arity n => Ord (Monomial RP.Lex n) where
+instance V.Arity n => Ord (Mon RP.Lex n) where
     compare a b = compare (degVec a) (degVec b)
 
-instance V.Arity n => Ord (Monomial RP.Glex n) where
+instance V.Arity n => Ord (Mon RP.Glex n) where
     compare a b = let aVb = compare (totalDegree a) (totalDegree b)
                   in  if aVb == EQ
                       then compare (degVec a) (degVec b)
                       else aVb
 
-instance V.Arity n => Ord (Monomial RP.GRevLex n) where
+instance V.Arity n => Ord (Mon RP.GRevLex n) where
     compare a b = let aVb = compare (totalDegree a) (totalDegree b)
                       a' = V.reverse $ degVec a
                       b' = V.reverse $ degVec b
@@ -33,24 +35,24 @@ instance V.Arity n => Ord (Monomial RP.GRevLex n) where
                       then compare b' a'
                       else aVb
 
-instance V.Arity n => Show (Monomial o n) where
+instance V.Arity n => Show (Mon o n) where
     show m = monListToString (V.toList $ degVec m)
 
-instance V.Arity n => Semigroup (Monomial o n) where
-    a <> b = Monomial { degVec = V.zipWith (+) (degVec a) (degVec b) }
+instance V.Arity n => Semigroup (Mon o n) where
+    a <> b = MakeMon { degVec = V.zipWith (+) (degVec a) (degVec b) }
 
-instance (KnownNat n, V.Arity n) => Monoid (Monomial o n) where
-    mempty = Monomial $ V.fromList' $ take nn (repeat 0)
+instance (KnownNat n, V.Arity n) => Monoid (Mon o n) where
+    mempty = MakeMon $ V.fromList' $ take nn (repeat 0)
         where nn = (fromInteger . reflect) (Proxy :: Proxy n)
 
-instance (KnownNat n, V.Arity n) => Readable (Monomial o n) where
-    fromString :: forall o n. (KnownNat n, V.Arity n) => String -> Monomial o n
-    fromString s = Monomial { degVec = V.fromList' $ monListFromString nn s }
+instance (KnownNat n, V.Arity n) => Readable (Mon o n) where
+    fromString :: forall o n. (KnownNat n, V.Arity n) => String -> Mon o n
+    fromString s = MakeMon { degVec = V.fromList' $ monListFromString nn s }
         where nn = (fromInteger . reflect) (Proxy :: Proxy n)
 
-totalDegree :: V.Arity n => Monomial o n -> Int
+totalDegree :: V.Arity n => Mon o n -> Int
 totalDegree = V.sum . degVec
 
-multiDegree :: V.Arity n => Maybe (Monomial o n) -> Maybe [Int]
+multiDegree :: V.Arity n => Maybe (Mon o n) -> Maybe [Int]
 multiDegree Nothing = Nothing
 multiDegree (Just m) = (Just . V.toList . degVec) m
