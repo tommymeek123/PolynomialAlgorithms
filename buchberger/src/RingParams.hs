990b7eef985dcdf8954323d4efdb9438ca459506
thmeek1 <thmeek1@catamount.wcu.edu>
2022-12-19 17:54:47 -0500

    removed buchberger directory

--- buchberger/src/RingParams.hs
+++
@@ -1,6 +0,0 @@
-module RingParams ( Ring(..)
-                  , MonOrder(..)
-                  ) where
-
-data Ring = Q | Fp deriving (Eq,Read,Show)
-data MonOrder = Lex | Glex | GRevLex deriving (Eq,Read,Show)
